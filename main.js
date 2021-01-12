fe7a684a2729e2f4a5fc2d34041664eb7fedd223
Paul Tobiloba <48536008+Toby-Codes@users.noreply.github.com>
2021-01-12 17:37:39 +0100

    bug:/ fixed checkbox function

--- main.js
+++ main.js
@@ -1,21 +1,4 @@
-const name = prompt('What is your name');
-console.log(`welcome, ${name}`);
-
 function toggle() {
-    const elm = document.getElementsByClassName('navigation__checkbox');
-    elm.checked = true;
+    const checkbox = document.querySelector('#navi-toggle');
+    checkbox.checked = !checkbox.checked;
     };
-
-
-// const navLink = document.querySelectorAll(.navigation__link);
-// const checkBox = document.querySelector(.navigation__checkbox);
-
-// navLink.addEventListener('click', () => {
-//     console.log("btn clicked");
-//     checkBox.removeAttribute('checked');
-// })
-
-// //document.getElementById("checkbox").checked = true;
-
-
-// add
